a6d3057d0bdc6f00010b3033e8062e6f64d598ab
Dimitri Van Loey <dimitrivanloey@gmail.com>
2020-05-07 11:57:37 +0100

    adding functionality to the start button

--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ class Settings:
         self.ship_limit = 3
         #Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3000
+        self.bullet_width = 30
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         #Alien settings
         self.alien_speed = 1.5
-        self.fleet_drop_speed = 50
+        self.fleet_drop_speed = 5
         #fleet-direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
